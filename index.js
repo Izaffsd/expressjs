717fba4d7a83ce3b474314f973a04c6978caa31d
Muhd Iskandar <iskandar.isa@scicom.com.my>
2025-12-17 18:12:43 +0800

    [feat]: MySQL connection

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
+const db = require('./connection')
 
 // user sign up ( FE ) -> response -> ( BE ) request -> dapat data -> ( JSON )
 
@@ -10,7 +11,11 @@ app.use(bodyParser.json())
 const name = 'Iskan'
 
 app.get('/', (req, res) => {
-    res.send('Hessllo')
+    db.query("SELECT * FROM student", (err, result) => {
+        console.log(result, 'dd')
+        // console.log(err)
+    })
+    res.send('Hessllo', db)
 })
 
 app.get('/signin', (req, res) => {
@@ -20,8 +25,12 @@ app.get('/signin', (req, res) => {
 
 app.post('/signup', (req, res) => {
     // console.log(req.body) undefined
-    console.log( { requestFromOutside: req } ) // shows full request object
+    console.log( { requestFromOutside: req.body } ) // shows full request object
     res.send('sign up successful')
+})
+
+app.put('/replace', (req, res) => {
+    res.send('put request successful', { requestFromOutside: req.body.password })
 })
 
 app.listen(port, () => {
